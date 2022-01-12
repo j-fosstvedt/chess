265859808a973b90e32a2d025d6345353f6c4338
jorgen-tharaldsen-fosstvedt <j.fosstvedt@gmail.com>
2022-01-12 22:16:54 +0100

    made it like it should be, almost

--- chess.py
+++ chess.py
@@ -9,12 +9,26 @@ canvas = Canvas(root, width = width, height = width)
 canvas.pack()
 
 
-knightImage = ImageTk.PhotoImage(Image.open("horse.jpg"))
+whiteRookImage = ImageTk.PhotoImage(Image.open("whiteRook.jpg"))
+blackRookImage = ImageTk.PhotoImage(Image.open("blackRook.jpg"))
+whiteKnightImage = ImageTk.PhotoImage(Image.open("whiteHorse.jpg"))
+blackKnightImage = ImageTk.PhotoImage(Image.open("blackHorse.jpg"))
+whiteBishopImage = ImageTk.PhotoImage(Image.open("whiteBishop.jpg"))
+blackBishopImage = ImageTk.PhotoImage(Image.open("blackBishop.jpg"))
+whiteQueenImage = ImageTk.PhotoImage(Image.open("whiteQueen.jpg"))
+blackQueenImage = ImageTk.PhotoImage(Image.open("blackQueen.jpg"))
+whiteKingImage = ImageTk.PhotoImage(Image.open("whiteKing.jpg"))
+blackKingImage = ImageTk.PhotoImage(Image.open("blackKing.jpg"))
+whitePawnImage = ImageTk.PhotoImage(Image.open("whitePawn.jpg"))
+blackPawnImage = ImageTk.PhotoImage(Image.open("blackPawn.jpg"))
+
+pieceImages = [whitePawnImage, whiteKnightImage, whiteBishopImage, whiteRookImage, whiteQueenImage, whiteKingImage, blackPawnImage, blackKnightImage, blackBishopImage, blackRookImage, blackQueenImage, blackKingImage]
+
 blackSquareImage = ImageTk.PhotoImage(Image.open("blackSquare.png"))
 whiteSquareImage = ImageTk.PhotoImage(Image.open("whiteSquare.png"))
 
 c = [[0 for i in range(8)] for i in range(8)]
-pawn, night, bishop, rook, queen, king = (1, 2, 3, 4, 5, 6)
+whitePawn, whiteNight, whiteBishop, whiteRook, whiteQueen, whiteKing, blackPawn, blackNight, blackBishop, blackRook, blackQueen, blackKing = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 
 def spawnImage(xCoord, yCoord, img):
     canvas.create_image(translateXCoord(xCoord), translateYCoord(yCoord), anchor=NW, image=img)
@@ -51,40 +65,64 @@ def squareSetup():
             j += 1
 
 def pieceImageSetup():
-    spawnImage()
+    # White pieces
+    spawnImage(0, 0, whiteRookImage)
+    spawnImage(1, 0, whiteKnightImage)
+    spawnImage(2, 0, whiteBishopImage)
+    spawnImage(3, 0, whiteQueenImage)
+    spawnImage(4, 0, whiteKingImage)
+    spawnImage(5, 0, whiteBishopImage)
+    spawnImage(6, 0, whiteKnightImage)
+    spawnImage(7, 0, whiteRookImage)
+
+    for i in range(8):
+        spawnImage(i, 1, whitePawnImage)
+        i += 1
+    
+    # Black pieces
+    spawnImage(0, 7, blackRookImage)
+    spawnImage(1, 7, blackKnightImage)
+    spawnImage(2, 7, blackBishopImage)
+    spawnImage(3, 7, blackQueenImage)
+    spawnImage(4, 7, blackKingImage)
+    spawnImage(5, 7, blackBishopImage)
+    spawnImage(6, 7, blackKnightImage)
+    spawnImage(7, 7, blackRookImage)
+
+    for i in range(8):
+        spawnImage(i, 6, blackPawnImage)
+        i += 1
 
 # Sets up the pieces
 def pieceSetup():
     # White pieces
-    c[0][0] = rook
-    c[0][1] = night
-    c[0][2] = bishop
-    c[0][3] = queen
-    c[0][4] = king
-    c[0][5] = bishop
-    c[0][6] = night
-    c[0][7] = rook
+    c[0][0] = whiteRook
+    c[0][1] = whiteNight
+    c[0][2] = whiteBishop
+    c[0][3] = whiteQueen
+    c[0][4] = whiteKing
+    c[0][5] = whiteBishop
+    c[0][6] = whiteNight
+    c[0][7] = whiteRook
 
     # Pawns
-    i = 0
-    while i < 8:
-        c[1][i] = pawn
+    for i in range(8):
+        c[1][i] = whitePawn
         i += 1
 
     # Black pieces
-    c[7][0] = rook
-    c[7][1] = night
-    c[7][2] = bishop
-    c[7][3] = queen
-    c[7][4] = king
-    c[7][5] = bishop
-    c[7][6] = night
-    c[7][7] = rook
+    c[7][0] = blackRook
+    c[7][1] = blackNight
+    c[7][2] = blackBishop
+    c[7][3] = blackQueen
+    c[7][4] = blackKing
+    c[7][5] = blackBishop
+    c[7][6] = blackNight
+    c[7][7] = blackRook
 
     # Pawns
-    i = 0
-    while i < 8:
-        c[6][i] = pawn
+    for i in range(8):
+        c[6][i] = blackPawn
         i += 1
 
 def movePiece():
@@ -102,14 +140,17 @@ def movePiece():
     xMoveTo = int(rows.find(moveToLetter))
     yMoveTo = int(moveToString[1]) - 1
 
+    # Move the piece
     c[yMoveTo][xMoveTo] = c[yMoveFrom][xMoveFrom]
-    c[yMoveFrom][xMoveFrom] = 0
 
     # Draw the picture
-    canvas.create_image(translateXCoord(xMoveTo), translateYCoord(yMoveTo), anchor=NW, image=knightImage)
+    spawnImage(xMoveTo, yMoveTo, pieceImages[int(c[yMoveFrom][xMoveFrom]) - 1])
     spawnSquare(xMoveFrom, yMoveFrom)
+    
+    c[yMoveFrom][xMoveFrom] = 0
 
 squareSetup()
+pieceImageSetup()
 pieceSetup()
 
 # Play the game
